aeb9941d647b77f2a7dbad9e55448318ada87b73
Fadil Žilić <zfadil995@gmail.com>
2017-11-21 00:05:52 +0100

    SplashScreen details, AppCompat for Login.

--- app/src/main/java/ba/zfadil995/notitial/LoginActivity.java
+++ app/src/main/java/ba/zfadil995/notitial/LoginActivity.java
@@ -1,22 +1,21 @@
 package ba.zfadil995.notitial;
 
-import android.app.Activity;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 
-public class LoginActivity extends Activity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
         APIClient.Instance.InitAPIClient(this);
     }
 
-    public class LogIn extends AsyncTask<Void, Void, Void>{
+    public class LogIn extends AsyncTask<Void, Void, Void> {
 
         @Override
         protected Void doInBackground(Void... voids) {
@@ -25,16 +24,14 @@ public class LoginActivity extends Activity {
         }
     }
 
-    public void LogIn(View view){
+    public void LogIn(View view) {
         try {
             findViewById(R.id.studentIDField).setEnabled(false);
             findViewById(R.id.passwordField).setEnabled(false);
             new LogIn().execute();
-        }
-        finally {
+        } finally {
             findViewById(R.id.studentIDField).setEnabled(true);
             findViewById(R.id.passwordField).setEnabled(true);
         }
     }
-
 }
---
+++ app/src/main/java/ba/zfadil995/notitial/SplashActivity.java
@@ -0,0 +1,17 @@
+package ba.zfadil995.notitial;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        startActivity(new Intent(this, LoginActivity.class));
+        finish();
+    }
+
+}
