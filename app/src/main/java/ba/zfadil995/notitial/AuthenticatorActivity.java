6bcc06a232419d584ea0e4eb951b1ef98ad8d886
Fadil Žilić <zfadil995@gmail.com>
2017-12-05 23:25:17 +0100

    Firebase base code.

--- app/src/main/java/ba/zfadil995/notitial/AuthenticatorActivity.java
+++ app/src/main/java/ba/zfadil995/notitial/AuthenticatorActivity.java
@@ -8,11 +8,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 
+import com.google.firebase.iid.FirebaseInstanceId;
+
 public class AuthenticatorActivity extends AppCompatActivity {
 
     public static final String ACCOUNT_TYPE = "student";
     public static final String AUTH_TOKEN_TYPE = "full_access";
     public static final String IS_ADDING_NEW_ACCOUNT = "is_adding_new_account";
+    private static String StudentID;
+    private static String Password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +41,11 @@ public class AuthenticatorActivity extends AppCompatActivity {
             findViewById(R.id.studentIDField).setEnabled(false);
             findViewById(R.id.passwordField).setEnabled(false);
 
+            StudentID = ((EditText) findViewById(R.id.studentIDField)).getText().toString();
+            Password = ((EditText) findViewById(R.id.passwordField)).getText().toString();
+
+            String token = FirebaseInstanceId.getInstance().getToken();
+
             new LogIn().execute();
         } finally {
             findViewById(R.id.studentIDField).setEnabled(true);
@@ -47,12 +56,8 @@ public class AuthenticatorActivity extends AppCompatActivity {
     public class LogIn extends AsyncTask<Void, Void, Void> {
         @Override
         protected Void doInBackground(Void... params) {
-            final String studentID = ((EditText) findViewById(R.id.studentIDField)).getText().toString();
-            final String password = ((EditText) findViewById(R.id.passwordField)).getText().toString();
-
-            String authToken = APIClient.Instance.LogIn(studentID, password);
-
-            createAccount(studentID, password, authToken);
+            String authToken = APIClient.Instance.LogIn(StudentID, Password);
+            createAccount(StudentID, Password, authToken);
             return null;
         }
     }
